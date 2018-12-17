d2d5e6aa80c4d31e5aa9d27676b334f247d97bc1
bsaul <bradleysaul@gmail.com>
2018-12-16 19:04:53 -0500

    scraping mbbs website

--- programs/0b_scrape_mbbs_site.R
+++ programs/0b_scrape_mbbs_site.R
@@ -1,9 +1,11 @@
 #------------------------------------------------------------------------------#
-#
-#
-#
+#  TITLE: Import MBBS website data 
+#   DATE: 20181216
+#   PROG: B. Saul
+#   DESC: Scrape MBBS website data and collect data into analytic file
 #------------------------------------------------------------------------------#
 library(rvest)
+library(lubridate)
 library(dplyr)
 library(stringr)
 
@@ -56,9 +58,67 @@ get_data <- function(x, count_tables){
     mutate_if(is.factor, as.character)
 }
 
+get_data_post2003 <- function(html, year){
+  x <- html %>%
+    html_nodes("table") %>%
+    html_table(fill = TRUE) %>%
+    .[[1]]  %>%
+    ## Remove blank rows
+    filter(X1 != "") %>%
+    ## Remove (summary) columns after last route
+    select( -(min(which(.[1, ] == "")):ncol(.))) %>%
+    ## Remove the total row and below
+    filter(row_number() < which(str_detect(.$X1, regex("^total", ignore_case = TRUE)) ))
+  
+  route_info <- x %>%
+    filter(row_number() <  which(str_detect(.$X1, regex("^species", ignore_case = TRUE)) )) %>%
+    filter(row_number() != which(str_detect(.$X1, regex("^habitat", ignore_case = TRUE))))
+  
+  hold <- t(route_info) %>% 
+    .[2:nrow(.), ] %>% 
+    as.data.frame(stringsAsFactors = FALSE) %>%
+    setNames(trimws(str_replace(tolower(t(route_info)[1, ]), "\\%", ""))) %>%
+    ## Clean up
+    mutate(
+      date     = as.Date(sprintf("%s/%s", date, year), format = "%m/%d/%Y"),
+      vehicles = as.numeric(str_extract(vehicles, "[0-9]+")),
+      year     = year(date)
+    ) %>%
+    select(route_num = route, 
+           hab_hm = `h or m`, hab_p = p, hab_o = o, hab_b = b, 
+           hab_other = `*`,
+           everything()) %>%
+    mutate_at(
+      .vars = vars(starts_with("hab")),
+      .funs = funs(ifelse(max(as.numeric(.) > 1), as.numeric(.)/100, as.numeric(.) ))
+    )
+  
+  route_counts <-  x %>%
+    filter(row_number() > which(str_detect(.$X1, regex("^species", ignore_case = TRUE)) )) %>%
+    setNames(c("common_name", 1:(ncol(.) - 1) )) %>%
+    tidyr::gather(key = "route_num", value = "count", -common_name) %>%
+    mutate(
+      count = as.numeric(count)
+    )
+  
+  hold %>%
+    left_join(route_counts, by = "route_num")
+}
+
+
+
 #####
-get_year_index_html <- function(year){
-  read_html(sprintf("http://rhwiley.bio.unc.edu/mbbs/%sOrange/index.html", year))
+get_year_index_html <- function(year, base_url = "http://rhwiley.bio.unc.edu/mbbs/"){
+  
+  if(year %in% as.character(1999:2003)){
+    url <- sprintf("%s%sOrange/index.html", base_url, year)
+  } else if( year == "2004"){
+    url <- sprintf("%s%sOrange/orange_%s.html", base_url, year, year)
+  } else {
+    url <- sprintf("%s%sOrange/orange%s.html", base_url, year, year)
+  }
+  
+  read_html(url)
 }
 
 get_year_links <- function(year_page){
@@ -83,15 +143,44 @@ get_all_year_data <- function(year){
     count_tables <- 2:3
   }
   
-  get_year_index_html(year) %>% 
-    get_year_links() %>%
-    purrr::map_dfr(~ get_route_data(year, .x, count_tables))
+  yrhtml <- get_year_index_html(year)
+  
+  if(year %in% as.character(1999:2003)){
+    yrhtml %>% 
+      get_year_links() %>%
+      purrr::map_dfr(~ get_route_data(year, .x, count_tables))
+  } else {
+    yrhtml %>%
+      get_data_post2003(year)
+  }
+
 }
 
 
 
 ### Gather all the data
-years <- as.character(1999:2009)
-dt <- purrr::map_dfr(years, ~get_all_year_data(.x))
 
-dt
+
+
+
+## Collect all the data ####
+years <- as.character(1999:2009)
+
+old_mbbs_dt <- purrr::map_dfr(years, function(y){
+  x <- get_all_year_data(y)
+  if(y %in% as.character(1999:2003)){
+    x <- x %>% mutate(
+      date  = as.Date(date, format = "%B %d, %Y"),
+      year  = year(date),
+      route = str_replace(route, " -- ", ", "),
+      route_num = str_extract(route, "[1-9][0-9]*"))
+  }
+  return(x)
+})
+
+
+
+
+
+
+
