4ff93e80a91722ccf464d2b8e0db501f1de5f4e9
Bradley Saul <bradleysaul@fastmail.com>
2022-10-10 20:39:04 -0400

    add flake support

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A basic flake for the mbbs package";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = [ R ];
+      };
+    });
+}
+
