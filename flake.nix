fddb5d7f21186e9752c2f82c9583c455a93c5799
Bradley Saul <bradleysaul@fastmail.com>
2023-07-20 14:22:27 -0400

    update nix flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,53 @@
 {
-  description = "A flake for functional statistics blog";
+  description = "Mini-bird breeding survey data and R package";
   nixConfig = {
-    bash-prompt = "funstats> ";
+    bash-prompt = "🐦> ";
   };
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      # Use the same nixpkgs
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, gitignore }:
     flake-utils.lib.eachDefaultSystem (system: let
+      
       pkgs = nixpkgs.legacyPackages.${system};
+      inherit (gitignore.lib) gitignoreSource;
+
+      mbbsDeps = with pkgs.rPackages; [
+            purrr
+            stringr
+            lubridate
+            readr 
+            magrittr
+            dplyr
+            glue
+            assertthat
+            yaml
+          ];
+  
     in {
+
+      packages.mbbs = pkgs.rPackages.buildRPackage {
+          name = "mbbs";
+          src = gitignoreSource ./.;
+          propagatedBuildInputs = mbbsDeps;
+        };
+      
+      packages.default = self.packages.${system}.mbbs;
+
       devShells.default =  pkgs.mkShell {
         nativeBuildInputs = [ pkgs.bashInteractive ];
         buildInputs = [
           pkgs.R
-        ];
+          pkgs.rPackages.devtools
+          pkgs.rPackages.usethis
+        ] ++ mbbsDeps ;
       }; 
 
     });
