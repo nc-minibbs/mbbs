c472d55ca8d5429f7b8719d945a67d4136440610
IJBG <ivaragoulden@gmail.com>
2024-01-05 21:12:22 -0500

    name temp, starting work on stop level comments

---
+++ inst/scratch/process_early_ebird_stop_data.R
@@ -0,0 +1,5 @@
+#for processing stop-level information from ebird checklists
+
+#problem 1: that data is not currently stored in any column. Is it removed when we filter the ebird checklists?
+
+#comes from column Observation.Details, need that to be added back in to import_ebird_data. Added
