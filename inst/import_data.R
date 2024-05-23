49e7801e60b587345948527d5cd51e0d1555c823
IJBG <ivaragoulden@gmail.com>
2024-05-23 13:39:04 -0400

    add Orange 2018 rt 5

--- inst/import_data.R
+++ inst/import_data.R
@@ -16,7 +16,7 @@ etax <- get_ebird_taxonomy()
 
 # import Orange
 mbbs_orange <-
-  import_ebird_data("inst/extdata/MyEBirdData_Orange_20230713.csv") %>%
+  import_ebird_data("inst/extdata/MyEBirdData_Orange_20240523.csv") %>%
   prepare_mbbs_data(
     mbbs_site_dt =
       readr::read_csv("inst/extdata/orange_1999-2009_from_website.csv") %>%
