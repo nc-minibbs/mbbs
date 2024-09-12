cfdd918ed4fafc42e10434df96317d23fbd9e3d2
IJBG <ivaragoulden@gmail.com>
2024-09-12 10:25:11 -0400

    set up testing

--- tests/testthat/test_process_observers.R
+++ tests/testthat/test_process_observers.R
@@ -12,3 +12,5 @@
 # similarly needs a save = FALSE
 # for a save = TESTING where then the file is saved in the test location
 # and can be checked (automatically) for the presence of updated data.
+
+UpSurEv_tcs <- read.csv("tests/testthat/update_survey_events_test_cases.csv", header = TRUE)
