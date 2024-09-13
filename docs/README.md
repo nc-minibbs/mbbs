7eb9f0be3fa9e0dbf0201c55312250d678454c8d
Bradley Saul <bradleysaul@gmail.com>
2024-09-13 14:19:50 -0400

    Skeleton for MBBS data site (#90)
    * add nix formatter
    
    * trying out simple site builder
    
    * bump version
    
    * well maybe this
    
    * trying this out
    
    * example with data
    
    * fix path
    
    * fix run on branch

---
+++ docs/README.md
@@ -0,0 +1,5 @@
+---
+title: MBBS data
+---
+
+[mbbs_chatham_20240205.csv](data/mbbs_chatham_20240205.csv)
---
+++ pages.nix
@@ -0,0 +1,34 @@
+# nix derivation to build mbbs project site
+{ 
+  self,
+  pkgs ? import <nixpkgs> { },
+  gitignoreSource,
+}:
+with pkgs;
+
+let commit = if (self ? rev) then self.rev else "dirty";
+in
+
+stdenv.mkDerivation {
+  name = "pages";
+  src = gitignoreSource ./.;
+
+  nativeBuildInputs = [ pandoc ];
+
+  buildPhase = ''
+    ${pandoc}/bin/pandoc docs/README.md \
+          --from=markdown \
+          --to=html \
+          --output=index.html \
+          --standalone \
+          --template=docs/template.html \
+          --variable=gitcommit:${commit}
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    mkdir -p $out/data
+    cp index.html $out
+    cp inst/analysis_data/mbbs_chatham_20240205.csv $out/data/mbbs_chatham_20240205.csv
+  '';
+}
