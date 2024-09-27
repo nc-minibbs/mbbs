9540524cc0c5ab4a54188cff1af5097652dfad95
IJBG <IJBG@users.noreply.github.com>
2024-09-27 02:46:58 +0000

    Style code (GHA)

--- R/prepare_transcribed_paper.R
+++ R/prepare_transcribed_paper.R
@@ -12,27 +12,28 @@
 #' @returns transcribed_paper df of all the observations transcribed from the paper files,
 #'  returned just in case this function is called to set a variable.
 update_transcribed_paper <- function() {
-  
   # set path
-  path = "inst/stop-level/transcribed_paper_files/"
-  
+  path <- "inst/stop-level/transcribed_paper_files/"
+
   # load in the list of transcribed files
-  transcribed <- 
-    list.files(path = path, 
-               pattern = "transcribed_paper_files")
+  transcribed <-
+    list.files(
+      path = path,
+      pattern = "transcribed_paper_files"
+    )
   # load in species_code to common_name converter
   sp_code_converter <- read.csv("inst/stop-level/transcribed_paper_files/bird_code_to_common_name.csv") %>%
     dplyr::select(common_name, species_code)
   # get taxonomy
   taxonomy <- get_ebird_taxonomy()
-  
+
   # read in all the transcribed files
-  t1 <- read_transcribed_csv(filepath = paste0(path,transcribed[1]))
-  t2 <- read_transcribed_csv(filepath = paste0(path,transcribed[2]))
-  t3 <- read_transcribed_csv(filepath = paste0(path,transcribed[3])) %>%
+  t1 <- read_transcribed_csv(filepath = paste0(path, transcribed[1]))
+  t2 <- read_transcribed_csv(filepath = paste0(path, transcribed[2]))
+  t3 <- read_transcribed_csv(filepath = paste0(path, transcribed[3])) %>%
     dplyr::mutate(count = as.integer(count))
-  t4 <- read_transcribed_csv(filepath = paste0(path,transcribed[4]))
-  
+  t4 <- read_transcribed_csv(filepath = paste0(path, transcribed[4]))
+
   # check files against each other, report mis-matches
   mismatches <-
     rbind(
@@ -43,13 +44,13 @@ update_transcribed_paper <- function() {
       tpaper_find_mismatches(t2, t4, t2$transcriber[1], t4$transcriber[1]),
       tpaper_find_mismatches(t3, t4, t3$transcriber[1], t4$transcriber[1])
     )
-  
+
   # save mismatches
   write.csv(mismatches, "inst/stop-level/transcribed_paper_files/scratch_remaining_mistmatches.csv", row.names = FALSE)
-  
-  # add all the files together where they overlap! 
+
+  # add all the files together where they overlap!
   # assumes no mismatches remaining.
-  transcribed_paper_raw <- 
+  transcribed_paper_raw <-
     rbind(
       tpaper_find_overlap(t1, t2, t1$transcriber[1], t2$transcriber[1]),
       tpaper_find_overlap(t1, t3, t1$transcriber[1], t3$transcriber[1]),
@@ -58,33 +59,32 @@ update_transcribed_paper <- function() {
       tpaper_find_overlap(t2, t4, t2$transcriber[1], t4$transcriber[1]),
       tpaper_find_overlap(t3, t4, t3$transcriber[1], t4$transcriber[1])
     )
-  
+
   # check that everything was double entered, no problems.
   assertthat::assert_that(
     sum(transcribed_paper_raw$both == "yes") == nrow(transcribed_paper_raw)
   )
-  
+
   # set up proper columns for joining with other mbbs rows.
-  transcribed_paper <- 
+  transcribed_paper <-
     transcribed_paper_raw %>%
     dplyr::select(-both, -obs1, -obs2, -obs3) %>%
     mutate(source = "transcribed_paper") %>%
     dplyr::rename(route_num = route) %>%
-    #convert species code to common_name
+    # convert species code to common_name
     left_join(sp_code_converter, by = c("species_code")) %>%
-    #filter out accipiter sp.
+    # filter out accipiter sp.
     filter(!species_code == "Accipiter SP" & !species_code == "accipiter sp") %>%
-    #add taxonomy
+    # add taxonomy
     left_join(taxonomy, by = "common_name") %>%
-    #remove unnesecary columns
+    # remove unnesecary columns
     dplyr::select(-species_code, )
-  
+
   # write csv
   write.csv(transcribed_paper, "inst/extdata/stop_level_transcribed_paper.csv", row.names = FALSE)
   print("stop_level_transcribed_paper updated")
-  
-  transcribed_paper #return, just in case it's called that way.
-  
+
+  transcribed_paper # return, just in case it's called that way.
 }
 
 
@@ -94,14 +94,13 @@ update_transcribed_paper <- function() {
 #' @importFrom stringr str_extract
 #' @returns transcribed the df read in from the csv from the filepath
 read_transcribed_csv <- function(filepath = "inst/stop-level/transcribed_paper_files/transcribed_paper_files_GEL.csv") {
-  
-  name = str_extract(filepath, "[A-Z][A-Z][A-Z](_[0-9])?")
-  
-  transcribed <- 
+  name <- str_extract(filepath, "[A-Z][A-Z][A-Z](_[0-9])?")
+
+  transcribed <-
     read.csv(filepath, header = TRUE) %>%
     mutate(transcriber = name)
-  
-  transcribed #return
+
+  transcribed # return
 }
 
 
@@ -115,57 +114,60 @@ read_transcribed_csv <- function(filepath = "inst/stop-level/transcribed_paper_f
 #' @param source2 character
 #' @param overlap the list of who transcribed which files
 tpaper_find_mismatches <- function(
-    df1 = read.csv("inst/stop-level/transcribed_paper_files/transcribed_paper_files_GEL.csv"), 
-    df2 = read.csv("inst/stop-level/transcribed_paper_files/transcribed_paper_files_ADS.csv"), 
-    source1 = "GEL", 
+    df1 = read.csv("inst/stop-level/transcribed_paper_files/transcribed_paper_files_GEL.csv"),
+    df2 = read.csv("inst/stop-level/transcribed_paper_files/transcribed_paper_files_ADS.csv"),
+    source1 = "GEL",
     source2 = "ADS",
     overlap = read.csv("inst/stop-level/transcribed_paper_files/mbbs_paper_files_list.csv")) {
-  
-  #filter only to the rows where we expect overlap
+  # filter only to the rows where we expect overlap
   overlap <- overlap %>%
-    #pick out just the columns we need, including the columns "transcribed_by_SOURCE1|2"
+    # pick out just the columns we need, including the columns "transcribed_by_SOURCE1|2"
     dplyr::select(mbbs_county, route, year, contains(source1) | contains(source2)) %>%
-    #filter to rows where both sources transcribed that route-year, eg. no NAs
-    filter(if_all(everything(), ~!is.na(.))) %>%
-    #add a column that will be easy to filter on
+    # filter to rows where both sources transcribed that route-year, eg. no NAs
+    filter(if_all(everything(), ~ !is.na(.))) %>%
+    # add a column that will be easy to filter on
     mutate(both = "yes")
-  
-  #filter the dfs
+
+  # filter the dfs
   df1 <- df1 %>%
     left_join(overlap) %>%
-    filter(both == "yes") 
-  
+    filter(both == "yes")
+
   df2 <- df2 %>%
     left_join(overlap) %>%
-    filter(both == "yes") 
-  
-  #anti_join the dfs to find differences
+    filter(both == "yes")
+
+  # anti_join the dfs to find differences
   anti_1 <- df1 %>%
     anti_join(df2, by = c("mbbs_county", "year", "route", "stop_num", "species_code", "count")) %>%
-    dplyr::rename(species_code_s1 = species_code,
-                  count_s1 = count) %>%
+    dplyr::rename(
+      species_code_s1 = species_code,
+      count_s1 = count
+    ) %>%
     mutate(s1 = source1)
-  
+
   anti_2 <- df2 %>%
     anti_join(df1, by = c("mbbs_county", "year", "route", "stop_num", "species_code", "count")) %>%
-    dplyr::rename(species_code_s2 = species_code,
-                  count_s2 = count) %>%
+    dplyr::rename(
+      species_code_s2 = species_code,
+      count_s2 = count
+    ) %>%
     mutate(s2 = source2)
-  
-  #collate all the mismatches together
+
+  # collate all the mismatches together
   mismatches1 <- left_join(anti_1, anti_2, by = c("mbbs_county", "year", "route", "stop_num")) %>%
     dplyr::select(-contains("obs"), -contains("notes"), -contains("transcribe"))
   mismatches2 <- left_join(anti_2, anti_1, by = c("mbbs_county", "year", "route", "stop_num")) %>%
     dplyr::select(-contains("obs"), -contains("notes"), -contains("transcribe"))
   mismatches <- bind_rows(mismatches1, mismatches2) %>%
     dplyr::distinct()
-  
-  mismatches #return
+
+  mismatches # return
 }
 
 
 #' Returns the rows from df1 and df2 that had double entry
-#' @importFrom dplyr select if_all  mutate left_join filter 
+#' @importFrom dplyr select if_all  mutate left_join filter
 #' @importFrom tidyr contains everything
 #' @returns x df with only the rows from df1 and df2 that had double entry
 #' @param df1 first transcribed paper df
@@ -174,28 +176,27 @@ tpaper_find_mismatches <- function(
 #' @param source2 character
 #' @param overlap the list of who transcribed which files
 tpaper_find_overlap <- function(
-    df1 = read.csv("inst/stop-level/transcribed_paper_files/transcribed_paper_files_GEL.csv"), 
-    df2 = read.csv("inst/stop-level/transcribed_paper_files/transcribed_paper_files_ADS.csv"), 
-    source1 = "GEL", 
+    df1 = read.csv("inst/stop-level/transcribed_paper_files/transcribed_paper_files_GEL.csv"),
+    df2 = read.csv("inst/stop-level/transcribed_paper_files/transcribed_paper_files_ADS.csv"),
+    source1 = "GEL",
     source2 = "ADS",
     overlap = read.csv("inst/stop-level/transcribed_paper_files/mbbs_paper_files_list.csv")) {
-  
-  #filter only to the rows where we expect overlap between the two dfs
+  # filter only to the rows where we expect overlap between the two dfs
   overlap <- overlap %>%
-    #pick out just the columns we need, including the columns "transcribed_by_SOURCE1|2"
+    # pick out just the columns we need, including the columns "transcribed_by_SOURCE1|2"
     dplyr::select(mbbs_county, route, year, contains(source1) | contains(source2)) %>%
-    #filter to rows where both sources transcribed that route-year, eg. no NAs
-    filter(if_all(everything(), ~!is.na(.))) %>%
-    #add a column that will be easy to filter on
+    # filter to rows where both sources transcribed that route-year, eg. no NAs
+    filter(if_all(everything(), ~ !is.na(.))) %>%
+    # add a column that will be easy to filter on
     mutate(both = "yes")
-  
-  #because we assume all mismatches have been handled, 
-  #now filter only to rows which had double entry
-  #and the two dfs will be equivalent
+
+  # because we assume all mismatches have been handled,
+  # now filter only to rows which had double entry
+  # and the two dfs will be equivalent
   x <- df1 %>%
     left_join(overlap) %>%
     filter(both == "yes") %>%
     select(-contains("transcribe"))
-  
-  x #return
+
+  x # return
 }
--- R/stop-level.R
+++ R/stop-level.R
@@ -3,15 +3,15 @@
 #
 
 # if stop_level_species_comments needs to be updated:
-  # load("data/mbbs.rda")
-  # process_species_comments(mbbs)
+# load("data/mbbs.rda")
+# process_species_comments(mbbs)
 
 # if stop_level_hist_xls.csv needs to be updated:
-  # hist_xls_purrr_directories() %>%
-  #   write.csv(file = "inst/extdata/stop_level_hist_xls.csv", row.names = FALSE)
+# hist_xls_purrr_directories() %>%
+#   write.csv(file = "inst/extdata/stop_level_hist_xls.csv", row.names = FALSE)
 
-#if transcribed files needs to be updated:
-  #update_transcribed_paper()
+# if transcribed files needs to be updated:
+# update_transcribed_paper()
 
 
 #' Replaces mbbs route-level data with the more granular stop-level information
@@ -21,29 +21,29 @@
 #' @param mbbs Any mbbs dataset, either the whole survey area or one county
 granulate_to_stop <- function(mbbs) {
   # is this function name a little silly? yes hehe! Also specific
-  
+
   # read in stop-level data sourced from species_comments, hist_xls, and transcribed_paper
-  species_comments <- read.csv("inst/extdata/stop_level_species_comments.csv") #contains 0s
-  hist_xls <- read.csv("inst/extdata/stop_level_hist_xls.csv") #contains 0s
-  transcribed_paper <- read.csv("inst/extdata/stop_level_transcribed_paper.csv") #does NOT contain 0s
-  
+  species_comments <- read.csv("inst/extdata/stop_level_species_comments.csv") # contains 0s
+  hist_xls <- read.csv("inst/extdata/stop_level_hist_xls.csv") # contains 0s
+  transcribed_paper <- read.csv("inst/extdata/stop_level_transcribed_paper.csv") # does NOT contain 0s
+
   # bind together stop_level data sourced from inst/
   stop_level_external <- bind_rows(hist_xls, species_comments, transcribed_paper) %>%
     mbbs_generate_route_ID()
-  
+
   routes_with_ext_stop_level <- stop_level_external %>%
     group_by(mbbs_county, route_num, year) %>%
     summarize() %>%
     group_by(mbbs_county, route_num) %>%
     summarize(num_years = n())
-  
+
   # add in stop_num = 1 for routes that have all stops aside from stop 1 and have 20 records for the route
   # one way to filter for this may be to group by route_ID, year and look for routes where EITHER and stop_num is not 0 or where any common_name appears more than once. These will be routes with stop_level information.
   # stop_level_internal <- mbbs %>%
   #   function to add stop_num where stop_num == 1 is missing
   #   then filter to all rows where !is.na(stop_num)
   #   and that should give all the mbbs rows with stop level information.
-  
+
   # compile and compare to current mbbs routes - do counts match?
   # where counts DONT match, have .csv where that decision is recorded, and correct one way or another. Usually should default to the starting route-level mbbs. At the least check mbbs$source to see where error is
   # where counts DO match, replace route-level information with stop information in the overall mbbs/mbbs_county dataset that this function is given. mbbs$source == "granulated to stop"
@@ -54,12 +54,10 @@ granulate_to_stop <- function(mbbs) {
 
 #' Runs process_species_comments and saves the data as a .csv
 run_process_species_comments <- function() {
-  
-  #load in mbbs
+  # load in mbbs
   load("data/mbbs.rda")
-  
+
   species_comments <- process_species_comments(mbbs)
-  
+
   write.csv(species_comments, "inst/extdata/stop_level_species_comments.csv", row.names = FALSE)
-  
 }
