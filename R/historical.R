778ecbfde1bb7ef77e68fb5533c1c00e3a46a9e1
Bradley Saul <bradleysaul@fastmail.com>
2024-09-30 11:56:05 -0400

    update historical

--- R/historical.R
+++ R/historical.R
@@ -21,9 +21,10 @@ historical_cols <-
     hab_other   = "d",
     vehicles    = "i")
 
-#' Get the historical (pre-eBird) MBBS data
+#' Loads the historical (pre-eBird) MBBS data from disk
 #'
-#' This function simply loads the csv files in the `historical` data directory
+#' This function simply loads the csv files
+#' in the `historical` data directory
 #' as a `data.frame`.
 #'
 #' @importFrom stringr str_extract
@@ -31,21 +32,49 @@ historical_cols <-
 #' @importFrom readr read_csv
 #' @importFrom lubridate mdy
 #' @include config.R
-get_historical_data <- function() {
+load_historical_data <- function() {
   list.files(config$historical_data_dir) |>
     purrr::map(
       .f = ~ {
         file.path(config$historical_data_dir, .x) |>
-          system.file(package = "mbbs")() |>
+          # system.file(package = "mbbs")() |>
           readr::read_csv(
             col_types = historical_cols
           ) |>
           dplyr::mutate(
-            county = stringr::str_extract(.x, "orange|chatham|durham"),
-            date   = lubridate::mdy(date),
-            time   = as.character(time)
+            county = stringr::str_extract(.x, "orange|chatham|durham")
           )
       }
     ) |>
     dplyr::bind_rows()
+}
+
+#' Transform historical (pre-eBird) data
+#'
+#' @importFrom dplyr select starts_with
+#' @include utilities.R
+transform_historical_data <- function(historical) {
+  historical |>
+    dplyr::select(
+      # route field in source data is missing for some routes
+      # route id is created from county and route_num below
+      - "route",
+    ) |>
+    dplyr::mutate(
+      route = make_route_id(county, route_num),
+      date   = lubridate::mdy(date),
+      time   = as.character(time)
+    )
+}
+
+#' Get the historical (pre-eBird) data
+#'
+get_historical_data <- function() {
+  load_historical_data() |>
+    transform_historical_data() |>
+    dplyr::select(
+      # Drop the habitat data available for (some) routes in historical data.
+      # See: https://github.com/nc-minibbs/mbbs/issues/103
+      -dplyr::starts_with("hab")
+    )
 }
