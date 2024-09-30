e2ee735cebbde9b4bbbf2e4c542823be86b376df
Bradley Saul <bradleysaul@fastmail.com>
2024-09-30 17:03:59 -0400

    rename test file

--- tests/testthat/test_ebird_import.R
+++ tests/testthat/test_ebird_import.R
@@ -1,6 +1,10 @@
 # Testing functions in
-# import_ebird_data.R
+# ebird_import.R
 
+
+test_that("load_ebird_data runs", {
+  expect_no_error(load_ebird_data(x))
+})
 
 test_that("get_exclusions runs", {
   x <- system.file("excluded_submissions.yml", package = "mbbs")
