33b048f4968c5d9415b29a99c240cd7617d21a84
Bradley Saul <bradleysaul@fastmail.com>
2024-10-01 13:47:24 -0400

    add patterns

--- R/config.R
+++ R/config.R
@@ -20,5 +20,8 @@ config <- list(
   county_pattern = "orange|chatham|durham",
 
   # files
-  excluded_submissions = "excluded_submissions.yml"
+  excluded_submissions = "excluded_submissions.yml",
+
+  # Regex patterns
+  county_pattern = "orange|chatham|durham"
 )
