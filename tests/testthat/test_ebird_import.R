1983ddb5978451bb2ad1f731bf355243847cd2b0
Bradley Saul <bradleysaul@fastmail.com>
2024-10-03 09:03:59 -0400

    update tests

--- tests/testthat/test_ebird_import.R
+++ tests/testthat/test_ebird_import.R
@@ -3,14 +3,16 @@
 
 
 test_that("load_ebird_data runs", {
-  expect_no_error(load_ebird_data(x))
+  withr::local_dir("../..")
+  expect_no_error(load_ebird_data())
 })
 
 test_that("get_ebird_data runs", {
-  expect_no_error(get_ebird_data(x))
+  withr::local_dir("../..")
+  expect_no_error(get_ebird_data())
 })
 
 test_that("get_exclusions runs", {
-  x <- system.file("excluded_submissions.yml", package = "mbbs")
-  expect_no_error(get_exclusions(x))
+  withr::local_dir("../..") 
+  expect_no_error(get_exclusions())
 })
