da07bb175e2fb2d774b7b172fdaad1b3f84ddb1d
Bradley Saul <bradleysaul@fastmail.com>
2024-10-08 11:59:25 -0400

    WIP

--- R/mbbs.R
+++ R/mbbs.R
@@ -6,17 +6,24 @@
 #'
 create_stop_level <- function(config = config) {
 
-  historical <- get_historical_data()
-
-
-  # all_ebird <- get_ebird_data()
-  # stop_ebird <- all_ebird |>
-  #   dplyr::filter(!is.na(stop_num))
+  all_ebird <- get_ebird_data()
+  stop_ebird <- all_ebird |>
+    dplyr::filter(!is.na(stop_num))
 
   # # Waiting for #120
   # stop_xls <- get_stop_level_xls_data()
   # stop_xls
   # stop_obs <-
+}
+
+#' Create the route level dataset
+#'
+create_route_level <- function(stop_level_data, config = config) {
+
+  # Get the historical data.
+  # NOTE:
+  historical <- get_historical_data()
+
 }
 
 #' Conform taxonomy of MBBS data from different sources
