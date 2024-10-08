7e6b5adb5bb98d1f09cdd4799ea03ac9d185bce5
Bradley Saul <bradleysaul@fastmail.com>
2024-10-08 12:30:28 -0400

    working on stop level

--- R/mbbs.R
+++ R/mbbs.R
@@ -4,27 +4,73 @@
 
 #' Create the stop level dataset
 #'
-create_stop_level <- function(config = config) {
+create_stop_level_0 <- function(config = config) {
 
   all_ebird <- get_ebird_data()
   stop_ebird <- all_ebird |>
-    dplyr::filter(!is.na(stop_num))
-
+    dplyr::filter(!is.na(stop_num)) |>
+    dplyr::select(-submission, -nobservers, -obs_details, -comments)
   # # Waiting for #120
   # stop_xls <- get_stop_level_xls_data()
   # stop_xls
   # stop_obs <-
+
+  stop_ebird
+}
+
+create_stop_level <- function(config = config) {
+
+  df <- create_stop_level_0(config = config)
+
+  yrs_in <- dplyr::distinct(df, year, route, stop_num) |>
+    arrange(year, route, stop_num)
+
+  logger::log_trace("Preliminary stop-level data has {nrow(df)} observations.")
+
+  df |>
+  # For each year that a route was run,
+  # add 0 count for those species that were *not* observed
+  # in that route / stop / year.
+    tidyr::complete(
+      tidyr::nesting(
+        year, date, time, county, route, route_num, stop_num, lat, lon
+      ),
+      tidyr::nesting(common_name, sci_name),
+      fill = list(count = 0)
+    ) |>
+    (\(x) {
+
+      # Check that we didn't add observations for route-stop-years
+      # not in the input data.
+
+      # browser()
+      yrs_out <- dplyr::distinct(x, year, route, stop_num) |>
+        arrange(year, route, stop_num)
+
+      assertthat::assert_that(
+        identical(yrs_in, yrs_out),
+        msg =
+         "create_stop_level added or lost year/route/stops and shouldn't have."
+      )
+
+      logger::log_trace(
+        "Completing stop-level data added {nrow(x) - nrow(df)} 0 counts."
+      )
+      x
+    })()
+
 }
 
 #' Create the route level dataset
 #'
-create_route_level <- function(stop_level_data, config = config) {
+create_route_level_0 <- function(stop_level_data, config = config) {
 
   # Get the historical data.
   # NOTE:
   historical <- get_historical_data()
 
 }
+
 
 #' Conform taxonomy of MBBS data from different sources
 #'
