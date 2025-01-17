84c24052e518d5053702f6512d0935df9ad3e74d
IJBG <IJBG@users.noreply.github.com>
2025-01-17 20:42:50 +0000

    Style code (GHA)

--- R/survey-list.R
+++ R/survey-list.R
@@ -14,54 +14,56 @@
 #' @importFrom stringr str_to_lower
 #' @importFrom utils write.csv
 update_survey_list <- function(ebird, config, path = config$survey_list, save = TRUE) {
- 
   # load in survey list
   survey_list <- read.csv(file = path, header = TRUE)
-  
-  #browser()
-  
+
+  # browser()
+
   # generate list of new surveys found in ebird not yet on the survey list
   ebird_surveys <-
     ebird$comments |>
     # filter to only surveys not already on survey_list
     anti_join(survey_list, by = c("route", "year"))
-  
+
   # flag and stop function if any rows in ebird_surveys have all NA observers and that survey is not represented with a non-NA observer in another row. eg: we're truly missing observer data.
   assertthat::assert_that(
     confirm_observer_NA(ebird_surveys) == FALSE,
-    msg = "While updating survey list, at least one new survey found where there is NO observer information. Please see ebird_surveys in update_survey_list and track down error in ebird.")
-  
+    msg = "While updating survey list, at least one new survey found where there is NO observer information. Please see ebird_surveys in update_survey_list and track down error in ebird."
+  )
+
   # Checks have passed, so filter out surveys with all NA observers
-  ebird_surveys <- 
+  ebird_surveys <-
     ebird_surveys |>
     filter(!(is.na(obs1) == TRUE &
-               is.na(obs2) == TRUE &
-               is.na(obs3) == TRUE)) |>
+      is.na(obs2) == TRUE &
+      is.na(obs3) == TRUE)) |>
     # Modify the observer strings, all to title case and trim white space
-    mutate(obs1 = str_to_title(obs1),
-           obs2 = str_to_title(obs2),
-           obs3 = str_to_title(obs3),
-           obs1 = str_trim(obs1),
-           obs2 = str_trim(obs2),
-           obs3 = str_trim(obs3))
-  
+    mutate(
+      obs1 = str_to_title(obs1),
+      obs2 = str_to_title(obs2),
+      obs3 = str_to_title(obs3),
+      obs1 = str_trim(obs1),
+      obs2 = str_trim(obs2),
+      obs3 = str_trim(obs3)
+    )
+
   # Add any new observers to the conversion table
   update_observer_table(ebird_surveys, config, save = save)
   # This is called separate to ensure input still gets taken for updating the table
-  
+
   # Standardize obs1/obs2/obs3 based on the conversion table
-  ebird_surveys <- 
+  ebird_surveys <-
     ebird_surveys |>
     convert_based_on_observer_table(config) |>
     # select only the columns we need
     select(route, year, obs1, obs2, obs3) |>
     # check one last time that at least 1 observer is not NA,
-    # like if an error happened when converting based on the 
+    # like if an error happened when converting based on the
     # observer table
     assertthat::assert_that(
-      ##missing this statement, WIP
+      ## missing this statement, WIP
     )
-  
+
   # Add new surveys to survey list if they exist
   # And save the updated list if save = TRUE
   if (nrow(ebird_surveys) > 0) {
@@ -69,14 +71,14 @@ update_survey_list <- function(ebird, config, path = config$survey_list, save = 
       rbind(survey_list, ebird_surveys) |>
       arrange(route, year)
     if (save == TRUE) {
-      write.csv(survey_list, config$survey_list, row.names = FALSE) 
+      write.csv(survey_list, config$survey_list, row.names = FALSE)
     }
     # print message
     cat(nrow(ebird_surveys), "surveys have been added to survey_list")
   } else { # report that there were no new surveys
     cat("No new surveys to add to survey_list\\n")
   }
-  
+
   # save a version for testing as well.
   # write.csv(mbbs_survey_events, file = system.file("tests/testthat/update_survey_events_test_cases.csv", package = "mbbs"), row.names = FALSE)
 }
@@ -87,38 +89,38 @@ update_survey_list <- function(ebird, config, path = config$survey_list, save = 
 # called within update_survey_list so we have already removed
 # rows with NA observers that have observer data on survey_list
 confirm_observer_NA <- function(ebird_surveys) {
-  
   # check for any rows with all NA observers
   all_na_obs <- ebird_surveys %>%
     filter((is.na(obs1) == TRUE &
-              is.na(obs2) == TRUE &
-              is.na(obs3) == TRUE))
-  
-  #if no rows are all NA, go ahead and return 'FALSE'
-  if(nrow(all_na_obs) == 0) {
+      is.na(obs2) == TRUE &
+      is.na(obs3) == TRUE))
+
+  # if no rows are all NA, go ahead and return 'FALSE'
+  if (nrow(all_na_obs) == 0) {
     return(FALSE)
   }
-  
+
   # do these rows with NA observers have other rows with observer information? If so, we're fine. If not, ERROR and end function here.
-  for(i in 1:nrow(all_na_obs)) {
-    
-    same_survey_noNA <- 
+  for (i in 1:nrow(all_na_obs)) {
+    same_survey_noNA <-
       ebird_surveys |>
-      #remove the all_na_rows
+      # remove the all_na_rows
       filter(!(is.na(obs1) == TRUE &
-                is.na(obs2) == TRUE &
-                is.na(obs3) == TRUE)) |>
-      filter(year == all_na_obs$year[i],
-             route == all_na_obs$route[i])
-    
-    # If no other ebird_surveys row has the same year/route, 
+        is.na(obs2) == TRUE &
+        is.na(obs3) == TRUE)) |>
+      filter(
+        year == all_na_obs$year[i],
+        route == all_na_obs$route[i]
+      )
+
+    # If no other ebird_surveys row has the same year/route,
     # ERROR
-    if(nrow(same_survey_noNA) == 0) {
-      return(TRUE) 
+    if (nrow(same_survey_noNA) == 0) {
+      return(TRUE)
       # TRUE represents that we found a genuine case of observer information missing
     }
-    
-  # NAs have now been fully evaluated, if we haven't returned yet there are no NAs that are a problem
+
+    # NAs have now been fully evaluated, if we haven't returned yet there are no NAs that are a problem
     return(FALSE) # FALSE represents we found no genuine NAs
   }
 }
@@ -126,34 +128,35 @@ confirm_observer_NA <- function(ebird_surveys) {
 #' Add any new entries to the observer_conversion_table
 #' Does not return anything.
 update_observer_table <- function(ebird_surveys, config, save = TRUE) {
-  
-  observer_table = read.csv(config$observer_conversion_table, header = TRUE)
-  
+  observer_table <- read.csv(config$observer_conversion_table, header = TRUE)
+
   # get new names from ebird_surveys
-  obs_list <- c(ebird_surveys$obs1,
-                ebird_surveys$obs2,
-                ebird_surveys$obs3)
-  
+  obs_list <- c(
+    ebird_surveys$obs1,
+    ebird_surveys$obs2,
+    ebird_surveys$obs3
+  )
+
   obs_list <- unique(obs_list[!is.na(obs_list)])
-  
+
   # If a name is not yet on the conversion_table
   # take input for the output_name
   input_name <- "example"
   output_name <- "example"
   temp_row <- data.frame(input_name, output_name)
-  for(a in 1:length(obs_list)) {
+  for (a in 1:length(obs_list)) {
     if (observer_table %>% filter(input_name == obs_list[a]) %>% nrow() > 0) {
       # name is already on the list, do nothing
     } else {
       # name is not already on list, take input for the output name
       # pull the first letter of the input_name so we can check if anything similar
       # is already on the mini_table
-          first_letter <- (str_sub(obs_list[a], start = 1, end = 1))
-          pattern <- paste0("^[", toupper(first_letter), tolower(first_letter), "].*")
+      first_letter <- (str_sub(obs_list[a], start = 1, end = 1))
+      pattern <- paste0("^[", toupper(first_letter), tolower(first_letter), "].*")
       # extract matches for that first letter from the mini_table
-          letter_matches <- str_extract(observer_table$output_name, pattern)
-          letter_matches <- unique(letter_matches[!is.na(letter_matches)])
-      #take input
+      letter_matches <- str_extract(observer_table$output_name, pattern)
+      letter_matches <- unique(letter_matches[!is.na(letter_matches)])
+      # take input
       print("New observer name needs standardizing for the observer_conversion_table:")
       print(obs_list[a])
       print("Here are all standardized observers with the same first letter:")
@@ -161,12 +164,12 @@ update_observer_table <- function(ebird_surveys, config, save = TRUE) {
       print(paste0("What should <", obs_list[a], "> be converted to? Enter a standardized name:"))
       temp_row$input_name <- obs_list[a]
       temp_row$output_name <- readline(":")
-      
+
       # add to the observer_table
       observer_table <- rbind(observer_table, temp_row)
     } # end else statement
   } # end loop through the obs_list
-  
+
   if (save == TRUE) {
     # save the conversion table
     write.csv(observer_table, config$observer_conversion_table, row.names = FALSE)
@@ -174,13 +177,12 @@ update_observer_table <- function(ebird_surveys, config, save = TRUE) {
 }
 
 
-#' Take df, convert columns obs1/obs2/obs3 based on the 
+#' Take df, convert columns obs1/obs2/obs3 based on the
 #' observer_table and then return the df with corrected columns
 convert_based_on_observer_table <- function(ebird_surveys, config) {
-  
-  observer_table = read.csv(config$observer_conversion_table, header = TRUE)
-  
-  #convert names to the observer table's output_name (eg, correct typos)
+  observer_table <- read.csv(config$observer_conversion_table, header = TRUE)
+
+  # convert names to the observer table's output_name (eg, correct typos)
   ebird_surveys2 <- ebird_surveys %>%
     left_join(observer_table, by = c("obs1" = "input_name")) %>%
     mutate(obs1 = output_name) %>%
@@ -191,9 +193,7 @@ convert_based_on_observer_table <- function(ebird_surveys, config) {
     left_join(observer_table, by = c("obs3" = "input_name")) %>%
     mutate(obs3 = output_name) %>%
     dplyr::select(-output_name)
-  
-  #returns
-  ebird_surveys
-  
-}
 
+  # returns
+  ebird_surveys
+}
--- tests/testthat/test_survey_list.R
+++ tests/testthat/test_survey_list.R
@@ -7,9 +7,9 @@ test_row <- read.csv("survey_list_test_row.csv", header = TRUE)
 test_that("survey_list has no rows with NA observers", {
   na_observer_rows <- survey_list %>%
     dplyr::filter((is.na(obs1) == TRUE &
-                      is.na(obs2) == TRUE &
-                      is.na(obs3) == TRUE))
-  
+      is.na(obs2) == TRUE &
+      is.na(obs3) == TRUE))
+
   expect_no_error(
     assertthat::assert_that(
       nrow(na_observer_rows) == 0,
@@ -19,13 +19,13 @@ test_that("survey_list has no rows with NA observers", {
 })
 
 test_that("survey_list handles a new row as expected", {
-  
+
 })
 
-#test_that("update_observer_table runs without error", {
-#  
+# test_that("update_observer_table runs without error", {
+#
 #  expect_no_error(
 #    update_observer_table(test_row,
 #                               config,
 #                               save = FALSE))
-#})
+# })
