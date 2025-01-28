bae027c809ef3e77089398ec91b26f084c4fac98
Bradley Saul <bradleysaul@fastmail.com>
2025-01-28 11:30:44 -0500

    skip tests on ci

--- tests/testthat/test_ebird_import.R
+++ tests/testthat/test_ebird_import.R
@@ -10,11 +10,17 @@ test_that("load_ebird_data runs", {
 })
 
 test_that("get_ebird_data runs", {
+
+  testthat::skip_on_ci()
+
   withr::local_dir("../..")
   expect_no_error(get_ebird_data())
 })
 
 test_that("get_exclusions runs", {
+
+  testthat::skip_on_ci()
+
   withr::local_dir("../..")
   expect_no_error(get_exclusions())
 })
--- tests/testthat/test_historical.R
+++ tests/testthat/test_historical.R
@@ -2,6 +2,9 @@
 # historical.R
 
 test_that("get_historical_data runs", {
+
+  testthat::skip_on_ci()
+
   withr::local_dir("../..")
   expect_no_error(get_historical_data())
 })
