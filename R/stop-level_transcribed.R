62b5ce0e51d344e354fc61496d44699d83437bb9
IJBG <ivaragoulden@gmail.com>
2026-02-05 16:14:58 -0500

    fix error affecting 8 duplicated rows

--- R/stop-level_transcribed.R
+++ R/stop-level_transcribed.R
@@ -226,7 +226,6 @@ get_stop_level_transcribed <- function() {
       notes = readr::col_skip(),
       source = readr::col_character(),
       common_name = readr::col_character(),
-      tax_order = readr::col_skip(),
       sci_name = readr::col_skip()
     )
   ) |>
